e90b2413ff45a7cedf18ac3411fe1090f45056e3
Brian Ondieki <brianondieki@Brians-MacBook-Pro.local>
2020-09-16 23:03:02 -0500

    commit on master

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"net/http"
+	"text/template"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-//struct for employee
 type Employee struct {
 	Id   int
 	Name string
@@ -29,7 +31,93 @@ func dbConn() (db *sql.DB) {
 
 }
 
-func main() {
-	dbConn()
+var tmpl = template.Must(template.ParseGlob("form/*"))
 
+func Index(w http.ResponseWriter, r http.Request) {
+	db := dbConn()
+	selDB, err := db.Query("SELECT * FROM Employee ORDER BY id DESC")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	emp := Employee{}
+	res := []Employee{}
+
+	for selDB.Next() {
+		var id int
+		var city, name string
+
+		err = selDB.Scan(&id, &name, &city)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		emp.Id = id
+		emp.Name = name
+		emp.City = city
+
+		res = append(res, emp)
+	}
+
+	tmpl.ExecuteTemplate(w, "Index", res)
+	defer db.Close()
+}
+
+func Show(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	nId := r.URL.Query().Get("id")
+	selDB, err := db.Query("SELECT * FROM Employee WHERE id=?", nId)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	emp := Employee{}
+
+	for selDB.Next() {
+		var id int
+		var name, city string
+
+		err := selDB.Scan(&id, &name, &city)
+		if err != nil {
+			panic(err.Error())
+		}
+		emp.Id = id
+		emp.City = city
+		emp.Name = name
+	}
+
+	tmpl.ExecuteTemplate(w, "Show", emp)
+	defer db.Close()
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+	tmpl.ExecuteTemplate(w, "New", nil)
+}
+
+func main() {
+	db := dbConn()
+
+	selDB, err := db.Query("SELECT * FROM Employee ORDER BY id DESC")
+	if err != nil {
+		panic(err.Error())
+	}
+	emp := Employee{}
+	res := []Employee{}
+
+	for selDB.Next() {
+		var id int
+		var name, city string
+		err = selDB.Scan(&id, &name, &city)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		emp.Id = id
+		emp.Name = name
+		emp.City = city
+
+		res = append(res, emp)
+	}
+
+	fmt.Println(res)
 }
